de1b5fb5ae684634804de8d833eb3bccf3e5ca45
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-09-23 17:53:43 -0400

    Executar o Job COMPLETED mais de 1 vez

--- src/main/java/com/softwarefoundation/springbatchapp/BatchConfig.java
+++ src/main/java/com/softwarefoundation/springbatchapp/BatchConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -21,29 +22,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
-
+    
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
-
+    
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
-
+    
     @Bean
     public Job imprimirTextoJob() {
         return jobBuilderFactory
                 .get("Nome do Job")
                 .start(imprimeTextoStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
-
+    
     public Step imprimeTextoStep() {
         return stepBuilderFactory.
                 get("Nome do Step")
                 .tasklet(getTasklet())
                 .build();
-
+        
     }
-
+    
     private Tasklet getTasklet() {
         Tasklet tasklet = new Tasklet() {
             @Override
@@ -54,5 +56,5 @@ public class BatchConfig {
         };
         return tasklet;
     }
-
+    
 }
