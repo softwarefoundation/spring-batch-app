8e24e055096e2eeacb6c8d800d67c3bd9e58dc98
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-09-23 17:59:56 -0400

    Recuperando parâmetros de execução na tasklet

--- src/main/java/com/softwarefoundation/springbatchapp/BatchConfig.java
+++ src/main/java/com/softwarefoundation/springbatchapp/BatchConfig.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,13 +24,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
-    
+
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
-    
+
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
-    
+
     @Bean
     public Job imprimirTextoJob() {
         return jobBuilderFactory
@@ -37,24 +39,27 @@ public class BatchConfig {
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
-    
+
     public Step imprimeTextoStep() {
         return stepBuilderFactory.
                 get("Nome do Step")
-                .tasklet(getTasklet())
+                .tasklet(getTasklet(null))
                 .build();
-        
+
     }
-    
-    private Tasklet getTasklet() {
+
+    @Bean
+    @StepScope
+    public Tasklet getTasklet(@Value("#{jobParameters['nome']}") String nome) {
         Tasklet tasklet = new Tasklet() {
             @Override
             public RepeatStatus execute(StepContribution sc, ChunkContext cc) throws Exception {
                 System.out.println(">>>> Spring Batch >>>>>");
+                System.out.println(String.format("Olá, %s", nome));
                 return RepeatStatus.FINISHED;
             }
         };
         return tasklet;
     }
-    
+
 }
